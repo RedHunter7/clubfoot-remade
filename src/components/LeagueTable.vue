4ecb41339b323bac9a36c50362dfc7e3291b59e8
redhunter7 <adlirafif42121@gmail.com>
2025-11-19 21:26:25 +0700

    add league page

---
+++ src/components/LeagueTable.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const rows = []
+for (let index = 0; index < 20; index++) {
+  rows[index] = index
+}
+</script>
+
+<template>
+  <div className="overflow-x-scroll h-full">
+    <table className="table">
+      <thead class="text-white">
+        <tr>
+          <th>#</th>
+          <th>Club</th>
+          <th>P</th>
+          <th>W</th>
+          <th>D</th>
+          <th>L</th>
+          <th>GD</th>
+          <th>PTS</th>
+        </tr>
+      </thead>
+      <tbody class="text-white">
+        <tr
+          v-for="row in rows"
+          :key="row"
+          class="odd:bg-base-300/10 hover:text-secondary odd:hover:bg-white even:hover:bg-primary-content"
+        >
+          <th>1</th>
+          <td>Manchester United</td>
+          <td>38</td>
+          <td>38</td>
+          <td>38</td>
+          <td>38</td>
+          <td>-100</td>
+          <td>114</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- src/views/LeagueView.vue
+++ src/views/LeagueView.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+import LeagueTable from '@/components/LeagueTable.vue'
+</script>
+
 <template>
   <div class="bg-primary">
-    <div class="flex flex-row h-screen">
-      <div class="w-1/3"></div>
-      <div class="w-2/3"></div>
+    <div class="flex flex-row h-[calc(100vh_-_72px)]">
+      <div class="w-2/5 my-8">
+        <div class="bg-white w-3/4 h-48 rounded-xl mx-auto"></div>
+        <div class="text-center text-white text-2xl my-4">
+          <div>Defending Champions</div>
+          <img src="@/assets/images/custom-club.svg" class="fill-white" srcset="" />
+          <div>Liverpool</div>
+        </div>
+      </div>
+      <div class="w-3/5">
+        <league-table />
+      </div>
     </div>
   </div>
 </template>
